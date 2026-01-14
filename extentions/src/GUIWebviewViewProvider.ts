5769b91de80a2e114be3961ab7d40845a11b6ed2
rooney <bch3454@gmail.com>
2026-01-14 15:56:46 +0900

    refector: GUIWebviewProvider.ts 정리

---
+++ extentions/src/GUIWebviewViewProvider.ts
@@ -0,0 +1,125 @@
+import * as vscode from "vscode";
+
+const CONFIG = {
+  devServer: {
+    port: 5173,
+    baseUrl: "http://localhost:5173",
+  },
+  paths: {
+    gui: "gui",
+    assets: "assets",
+  },
+  files: {
+    script: "index.js",
+    style: "index.css",
+    scriptSource: "src/main.tsx",
+    styleSource: "src/index.css",
+  },
+} as const;
+
+interface ResourceUris {
+  scriptUri: string;
+  styleMainUri: string;
+  vscMediaUrl: string;
+}
+
+export class GUIWebviewViewProvider implements vscode.WebviewViewProvider {
+  public static readonly viewType = "bui-plugin-view";
+
+  constructor(
+    private readonly _extensionUri: vscode.Uri,
+    private readonly _extensionContext: vscode.ExtensionContext
+  ) {}
+
+  resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    _context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    this.configureWebviewOptions(webviewView.webview);
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+  }
+
+  private get isDevelopmentMode(): boolean {
+    return (
+      this._extensionContext.extensionMode === vscode.ExtensionMode.Development
+    );
+  }
+
+  private configureWebviewOptions(webview: vscode.Webview): void {
+    webview.options = {
+      enableScripts: true,
+      localResourceRoots: [
+        this._extensionUri,
+        vscode.Uri.joinPath(this._extensionUri, CONFIG.paths.gui),
+      ],
+      portMapping: this.isDevelopmentMode
+        ? [
+            {
+              webviewPort: CONFIG.devServer.port,
+              extensionHostPort: CONFIG.devServer.port,
+            },
+          ]
+        : undefined,
+    };
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview): string {
+    const resourceUris = this._getResourceUris(webview);
+
+    return `<!DOCTYPE html>
+    <html lang="ko">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script>const vscode = acquireVsCodeApi();</script>
+        <script>window.vscMediaUrl = "${resourceUris.vscMediaUrl}"</script>
+        <link href="${resourceUris.styleMainUri}" rel="stylesheet">
+        <title>BUI Plugin</title>
+      </head>
+      <body>
+        <div id="root"></div>
+        ${this._getReactRefreshScript()}
+        <script type="module" src="${resourceUris.scriptUri}"></script>
+      </body>
+    </html>`;
+  }
+
+  private _getResourceUris(webview: vscode.Webview): ResourceUris {
+    if (this.isDevelopmentMode) {
+      return {
+        scriptUri: `${CONFIG.devServer.baseUrl}/${CONFIG.files.scriptSource}`,
+        styleMainUri: `${CONFIG.devServer.baseUrl}/${CONFIG.files.styleSource}`,
+        vscMediaUrl: CONFIG.devServer.baseUrl,
+      };
+    }
+
+    // 프로덕션 모드: 빌드된 파일 사용
+    const guiUri = vscode.Uri.joinPath(this._extensionUri, CONFIG.paths.gui);
+    const assetsUri = vscode.Uri.joinPath(guiUri, CONFIG.paths.assets);
+
+    return {
+      scriptUri: webview
+        .asWebviewUri(vscode.Uri.joinPath(assetsUri, CONFIG.files.script))
+        .toString(),
+      styleMainUri: webview
+        .asWebviewUri(vscode.Uri.joinPath(assetsUri, CONFIG.files.style))
+        .toString(),
+      vscMediaUrl: webview.asWebviewUri(guiUri).toString(),
+    };
+  }
+
+  private _getReactRefreshScript(): string {
+    if (!this.isDevelopmentMode) {
+      return "";
+    }
+
+    return `<script type="module">
+          import RefreshRuntime from "${CONFIG.devServer.baseUrl}/@react-refresh"
+          RefreshRuntime.injectIntoGlobalHook(window)
+          window.$RefreshReg$ = () => {}
+          window.$RefreshSig$ = () => (type) => type
+          window.__vite_plugin_react_preamble_installed__ = true
+          </script>`;
+  }
+}
--- extentions/src/extension.ts
+++ extentions/src/extension.ts
@@ -1,129 +1,17 @@
 import * as vscode from "vscode";
-
-class BuiPluginViewProvider implements vscode.WebviewViewProvider {
-  public static readonly viewType = "bui-plugin-view";
-  private _view?: vscode.WebviewView;
-
-  constructor(
-    private readonly _extensionUri: vscode.Uri,
-    private readonly _extensionContext: vscode.ExtensionContext
-  ) {}
-
-  resolveWebviewView(
-    webviewView: vscode.WebviewView,
-    context: vscode.WebviewViewResolveContext,
-    _token: vscode.CancellationToken
-  ) {
-    this._view = webviewView;
-
-    const inDevelopmentMode =
-      this._extensionContext.extensionMode === vscode.ExtensionMode.Development;
-    console.log(
-      "===============inDevelopmentMode===============",
-      inDevelopmentMode
-    );
-
-    webviewView.webview.options = {
-      enableScripts: true,
-      localResourceRoots: [
-        this._extensionUri,
-        vscode.Uri.joinPath(this._extensionUri, "gui"),
-      ],
-      portMapping: inDevelopmentMode
-        ? [
-            {
-              webviewPort: 5173,
-              extensionHostPort: 5173,
-            },
-          ]
-        : undefined,
-    };
-    webviewView.webview.html = this._getHtmlForWebview(
-      webviewView.webview,
-      inDevelopmentMode
-    );
-  }
-
-  private _getHtmlForWebview(
-    webview: vscode.Webview,
-    inDevelopmentMode: boolean
-  ) {
-    let scriptUri: string;
-    let styleMainUri: string;
-    let vscMediaUrl: string;
-
-    if (inDevelopmentMode) {
-      // 개발 모드: Vite 개발 서버 사용
-      scriptUri = "http://localhost:5173/src/main.tsx";
-      styleMainUri = "http://localhost:5173/src/index.css";
-      vscMediaUrl = "http://localhost:5173";
-    } else {
-      // 프로덕션 모드: 빌드된 파일 사용
-      scriptUri = webview
-        .asWebviewUri(
-          vscode.Uri.joinPath(this._extensionUri, "gui", "assets", "index.js")
-        )
-        .toString();
-      styleMainUri = webview
-        .asWebviewUri(
-          vscode.Uri.joinPath(this._extensionUri, "gui", "assets", "index.css")
-        )
-        .toString();
-      vscMediaUrl = webview
-        .asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "gui"))
-        .toString();
-    }
-
-    return `<!DOCTYPE html>
-    <html lang="ko">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script>const vscode = acquireVsCodeApi();</script>
-        <script>window.vscMediaUrl = "${vscMediaUrl}"</script>
-        <link href="${styleMainUri}" rel="stylesheet">
-        <title>BUI Plugin</title>
-      </head>
-      <body>
-        <div id="root"></div>
-        
-        ${
-          inDevelopmentMode
-            ? `<script type="module">
-          import RefreshRuntime from "http://localhost:5173/@react-refresh"
-          RefreshRuntime.injectIntoGlobalHook(window)
-          window.$RefreshReg$ = () => {}
-          window.$RefreshSig$ = () => (type) => type
-          window.__vite_plugin_react_preamble_installed__ = true
-          </script>`
-            : ""
-        }
-        <script type="module" src="${scriptUri}"></script>
-      </body>
-    </html>`;
-  }
-}
+import { GUIWebviewViewProvider } from "./GUIWebviewViewProvider";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "bui-plugin" is now active!');
 
-  const provider = new BuiPluginViewProvider(context.extensionUri, context);
+  const provider = new GUIWebviewViewProvider(context.extensionUri, context);
 
   context.subscriptions.push(
     vscode.window.registerWebviewViewProvider(
-      BuiPluginViewProvider.viewType,
+      GUIWebviewViewProvider.viewType,
       provider
     )
   );
-
-  const disposable = vscode.commands.registerCommand(
-    "bui-plugin.helloWorld",
-    () => {
-      vscode.window.showInformationMessage("Hello World from bui-plugin!");
-    }
-  );
-
-  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
